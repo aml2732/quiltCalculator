2024338f7524cfc3b5dbc32fd456860fff52e414
unknown <aml2732@utexas.edu>
2024-04-13 22:16:15 -0500

    Updates

---
+++ precache-manifest.29411a9f038a34e23a2d5858dc853759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637a338f90343e2cb6bbf9d5828d5432",
+    "url": "/quiltCalculator/index.html"
+  },
+  {
+    "revision": "d3d7c548111794aa8b56",
+    "url": "/quiltCalculator/static/css/main.433a33b5.chunk.css"
+  },
+  {
+    "revision": "9d81a0834944d2370c70",
+    "url": "/quiltCalculator/static/js/2.e992bd1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiltCalculator/static/js/2.e992bd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d7c548111794aa8b56",
+    "url": "/quiltCalculator/static/js/main.85c34dca.chunk.js"
+  },
+  {
+    "revision": "f3df636a4e2c17d31c71",
+    "url": "/quiltCalculator/static/js/runtime-main.14753901.js"
+  }
+]);
